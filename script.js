68b2b66145421d5e82d7eca94f1a6c0b0eda62fa
BARATH0213 <barathm1710@gmail.com>
2024-06-30 15:50:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// scripts.js
+const btn = document.getElementById("submit");
+
+btn.addEventListener("click", function() {
+  alert("email has been sended successfully...")
+  console.log("btn")
+});
